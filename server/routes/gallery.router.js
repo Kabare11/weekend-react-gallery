642c3ae1357c45f0a425decd9fe8bae5e9e06f20
Abdulahi Kabare <abdullahikabare@gmail.com>
2024-01-15 03:16:13 -0600

    ..

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,13 +4,39 @@ const router = express.Router();
 
 
 // PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
+
+router.put('gallery/like:id', (req, res) => {
+
   // code here
-});
+  let idToGet = req.params.id
+  console.log("itemID", idToGet)
+  const queryText = `UPDATE "items" SET "isPurchased"= TRUE WHERE "id" = $1;`
+  const queryParam = [idToGet]
+  pool.query(queryText, queryParam)
+    .then((result) => {
+      res.sendStatus(204)
+    })
+    .catch((error) => {
+      console.log("Woops, error making query: ", queryText)
+      console.error(error)
+      res.sendStatus(500)
+    })
+})
+
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  console.log("In GET request");
+  let queryText = 'SELECT * from "gallery"';
+
+  pool.query(queryText).then((result) => {
+    res.send(result.rows);
+  }).catch((err) => {
+    console.log(err);
+    res.sendStatus(500);
+  })
 });
+
 
 module.exports = router;
---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./GalleryItem"
+
+function Gallery() {
+  return (
+    
+  );
+}
+
+export default Gallery;
